6254713770670769ec414def276d14dcc249cc5f
ivantime <sayivanthreetimes@gmail.com>
2022-06-22 19:02:22 +0800

    refractored code, with grouping of elements (nodes,images) and show selection of node on hover over

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Added:
 -Arrows on Links to Show Infection Chain (Extra x2 Challenge)
 -Curved/Non-Straight Links (Extra x2 Challenge?)
 -Show Information of Nodes (Patient) on Hover Over/Out via Tooltip (Extra Challenge)
+-Show "Selection" of Nodes (Patient) on Hover Over/Out via Tooltip (Extra Challenge)
 
 Missing:
--Show "Selection" of Nodes (Patient) on Hover Over/Out via Tooltip
 -Add 1 more category (eg. age/vaccine status)
 
 According to: https://chi-loong.github.io/CSC3007/assignment4.html
--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 let width = 800, height = 800;
 
-let svg = d3.select("svg")
-    // .attr("viewBox", "0 0 " + width + " " + height)
-    .attr("viewBox", `${-width / 4} ${-height / 8} ${width * 2} ${height * 2}`)
-    .attr("preserveAspectRatio", "xMinYMin meet")
+d3.select("#m").property('checked', false);
+d3.select("#f").property('checked', false);
+d3.select("#a").property('checked', true);
 
-// Load external data
-Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(data => {
+function drawDiagram(data) {
 
-    // Data preprocessing
-    data[0].forEach((e) => {
-        e.source = e.infector;
-        e.target = e.infectee;
-    });
+
+    let svg = d3.select("svg")
+        // .attr("viewBox", "0 0 " + width + " " + height)
+        .attr("viewBox", `${-width / 4} ${-height / 8} ${width * 2} ${height * 2}`)
+        .attr("preserveAspectRatio", "xMinYMin meet")
+
+    data[0].forEach((d0) => {
+        d0.target = d0.infectee
+        d0.source = d0.infector
+    })
+
+    radius = 30;
+
+    let linkpath = svg.append("g")
+        .attr("id", "links")
+        .selectAll("path")
+        .data(data[0])
+        .enter()
+        .append("path")
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 5)
+        .attr("marker-end", "url(#point-arrow)");
+
+    var groups = svg.selectAll("g.nodes")
+        .data(data[1])
+        .enter()
+        .append("g")
+        .attr('class', 'nodes')
+        .attr("gender", function (d) { return d.gender })
+
     svg.append("svg:defs").selectAll("marker")
         .data(["point-arrow"])      // Different link/path types can be defined here
         .enter().append("svg:marker")    // This section adds in the arrows
@@ -26,37 +50,68 @@ Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(d
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
 
-    let linkpath = svg.append("g")
-        .attr("id", "links")
-        .selectAll("path")
-        .data(data[0])
-        .enter()
-        .append("path")
-        .attr("fill", "none")
-        .attr("stroke", "black")
-        .attr("stroke-width", 5)
-        .attr("marker-end", "url(#point-arrow)");
+    svg.selectAll("g.nodes")
+        .each(function (d, i) {
 
 
-    radius = 30;
+            d3.select(this)
+                .append("circle")
+                .attr("id", "icons")
+                .attr("id", function (d) { return d.id })
+                .attr("stroke", "black")
+                .attr("stroke-width", 3)
+                .attr("pointer-events", "visible")
 
-    let node = svg.append("g")
-        .selectAll("circle")
-        .data(data[1])
-        .attr("id", "icons")
-        .enter()
-        .append("circle")
-        .attr("id", function (d) { return d.id })
-        .attr("pointer-events", "visible")
+                .attr("r", radius)
+                .attr("fill", function (d) {
+                    return (d.gender === "male" ? "lightblue" : "lightpink");
+                })
 
-        .attr("r", radius)
-        .attr("fill", function (d) {
-            return (d.gender === "male" ? "lightblue" : "lightpink");
+            d3.select(this)
+                .append("image")
+                .attr("xlink:href", 
+                "https://icon-library.com/images/person-icon-transparent-background/person-icon-transparent-background-9.jpg")
+                .attr("width", radius + 25)
+                .attr("height", radius + 25)
+
+
+
+        });
+
+    let node = svg.selectAll("g.nodes").selectAll("circle")
+    let svgIcons = svg.selectAll("g.nodes").selectAll("image")
+
+
+    groups
+        .on("mousemove", function (d, i) {
+            //add stroke for circle nodes to be more distinguished
+            d3.selectAll("circle").attr("stroke", "gray")
+            d3.selectAll("circle").attr("fill", "gray")
+
+            d3.select(this).select("circle").attr("stroke", "red")
+            d3.select(this).select("circle").attr("fill", function (d) { return (d.gender === "male" ? "lightblue" : "lightpink") })
+
+            div
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY) + "px")
+                .html(
+                    "Patient ID: " + i.id + "<br>" +
+                    "Age: " + i.age + "<br>" +
+                    "Gender: " + i.gender.toUpperCase() + "<br>" +
+                    "Nationality: " + i.nationality.toUpperCase() + "<br>" +
+                    "Occupation: " + i.occupation.toUpperCase() + "<br>" +
+                    "Organization: " + i.organization.toUpperCase() + "<br>" +
+                    "Date: " + i.date + "<br>" +
+                    "Vaccinated: " + i.vaccinated.toUpperCase() + "")
+                .style("opacity", 1);
         })
+        .on("mouseout", function (d) {
+            d3.selectAll("circle").attr("fill", function (d) { return (d.gender === "male" ? "lightblue" : "lightpink") })
+            d3.select(this).select("circle").attr("stroke", "black")
 
-        //add stroke for circle nodes to be more distinguished
-        .attr("stroke", "gray")
-        .attr("stroke-width", 4)
+            div
+                .style("opacity", 0);
+        })
         //add on drag of circles
         .call(d3.drag()
             .on("start", dragstarted)
@@ -74,73 +129,14 @@ Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(d
         .style('border-radius', '4px')
         .style('color', '#fff');
 
-
-    let svgIcons = svg.append("g")
-        .selectAll("image")
-        .data(data[1])
-        .enter()
-        .append("image")
-        .attr("id", function (d) { return d.id })
-        .attr("age", function (d) { return d.age })
-        .attr("gender", function (d) { return d.gender })
-        .attr("nationality", function (d) { return d.nationality })
-        .attr("occupation", function (d) { return d.occupation })
-        .attr("organization", function (d) { return d.organization })
-        .attr("date", function (d) { return d.date })
-        .attr("vaccinated", function (d) { return d.vaccinated })
-        .attr("xlink:href", "https://icon-library.com/images/person-icon-transparent-background/person-icon-transparent-background-9.jpg")
-        .attr("width", radius + 25)
-        .attr("height", radius + 25)
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended))
-        .on('mousemove', function (d, i) {
-            div
-                .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY) + "px")
-                .html(
-                    "Patient ID: " + d3.select(this).attr("id") + "<br>" +
-                    "Age: " + d3.select(this).attr("age") + "<br>" +
-                    "Gender: " + d3.select(this).attr("gender").toUpperCase() + "<br>" +
-                    "Nationality: " + d3.select(this).attr("nationality").toUpperCase() + "<br>" +
-                    "Occupation: " + d3.select(this).attr("occupation").toUpperCase() + "<br>" +
-                    "Organization: " + d3.select(this).attr("organization").toUpperCase() + "<br>" +
-                    "Date: " + d3.select(this).attr("date") + "<br>" +
-                    "Vaccinated: " + d3.select(this).attr("vaccinated").toUpperCase() + "")
-                .style("opacity", 1);
-        })
-        // d3.select(this)
-        //     .attr('fill', 'red')
-
-        //     //add outline
-        //     .attr("stroke", "red")
-        //     .attr("stroke-width", 5)
-        .on("mouseout", function (d) {
-            div
-                .style("opacity", 0);
-
-            // d3.select(this)
-            //     .attr('opacity', '1')
-            //     //add outline
-            //     .attr("stroke", "gray")
-            //     .attr("stroke-width", 5)
-
-
-
-        });
-
-
-    const simulation = d3.forceSimulation(data[1])
+    const simulation = d3.forceSimulation()
+        .nodes(data[1])
         .force("link", d3.forceLink(data[0]).id(d => d.id))
-        .force("charge", function (d) {
-            return d3.forceManyBody().strength(-300)
-
-        })
+        .force("charge", d3.forceManyBody().strength(-300))
         .force("x", d3.forceX())
         .force("y", d3.forceY())
         .force('center', d3.forceCenter(width / 2, height / 2))
-        .force('collide', d3.forceCollide(d => 65))
+        .force('collide', d3.forceCollide(65))
         .on("tick", d => {
 
             node
@@ -179,4 +175,9 @@ Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(d
         d.fy = null;
     }
 
+
+}
+// Load external data
+Promise.all([d3.json("links-sample.json"), d3.json("cases-sample.json")]).then(data => {
+    drawDiagram(data)
 })
